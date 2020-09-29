ec99ff7d35a0cad2102009b8d9a03ecc4bc6c147
Peter Hadeed <peterjhadeed1@gmail.com>
2020-09-29 12:20:29 -0400

    Hello.java changes

--- src/main/java/servlet/Hello.java
+++ src/main/java/servlet/Hello.java
@@ -1,24 +1,27 @@
-// Simple example servlet from slides
-import javax.servlet.*; // servlet library
-import javax.servlet.http.*; // servlet library
-import java.io.*;
+ // Simple example servlet from slides
+    import javax.servlet.*; // servlet library
+    import javax.servlet.http.*; // servlet library
+    import java.io.*;
+    import javax.servlet.annotation.WebServlet;
 
-public class Hello extends HttpServlet // Inheriting from HttpServlet makes this a servlet
-{
-public void doGet (HttpServletRequest request, HttpServletResponse response)
-                   throws ServletException, IOException
-{
-   response.setContentType("text/html"); // Tells the web container what we're sending back
-   PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
-
-   out.println("<html>");
-   out.println("<head>");
-   out.println("<title>Peter Hadeed's servlet</title>");
-   out.println("</head>");
-   out.println("<body>");
-   out.println("<p>Peter Hadeed's first servlet</p>");
-   out.println("</body>");
-   out.println("</html>");
-   out.close();
-}  // end doGet()
-}  // end Hello
+    //The @WebServletannotation is used to declare a servlet
+    @WebServlet(name = "Peter's First Servlet", urlPatterns = {"/Hello"})
+    public class Hello extends HttpServlet // Inheriting from HttpServlet makes this a servlet
+    {
+        public void doGet (HttpServletRequest request, HttpServletResponse response)
+                throws ServletException, IOException
+        {
+            response.setContentType("text/html"); // Tells the web container what we're sending back
+            PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
+     
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Peter Hadeed First Servlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>Peter's first servlet. Priviet!</p>");
+            out.println("</body>");
+            out.println("</html>");
+            out.close();
+        }  // end doGet()
+    }  // end Hello
