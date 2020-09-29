f927ac8d7c7ab23c7815f635d9235475c07b587e
Peter Hadeed <peterjhadeed1@gmail.com>
2020-09-29 12:00:28 -0400

    Procfile change for windows and adding Hello.java to servlets

---
+++ src/main/java/servlet/Hello.java
@@ -0,0 +1,24 @@
+// Simple example servlet from slides
+import javax.servlet.*; // servlet library
+import javax.servlet.http.*; // servlet library
+import java.io.*;
+
+public class Hello extends HttpServlet // Inheriting from HttpServlet makes this a servlet
+{
+public void doGet (HttpServletRequest request, HttpServletResponse response)
+                   throws ServletException, IOException
+{
+   response.setContentType("text/html"); // Tells the web container what we're sending back
+   PrintWriter out = response.getWriter(); // Make it appear as if we're "writing" to the browser window
+
+   out.println("<html>");
+   out.println("<head>");
+   out.println("<title>Peter Hadeed's servlet</title>");
+   out.println("</head>");
+   out.println("<body>");
+   out.println("<p>Peter Hadeed's first servlet</p>");
+   out.println("</body>");
+   out.println("</html>");
+   out.close();
+}  // end doGet()
+}  // end Hello
