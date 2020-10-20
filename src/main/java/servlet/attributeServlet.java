04bc0c7795f914ecbcc3b0eb8449c21e9ad703b6
Peter Hadeed <peterjhadeed1@gmail.com>
2020-10-20 19:17:05 -0400

    In-class

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -15,11 +15,53 @@ public class attributeServlet extends HttpServlet
 public void doGet (HttpServletRequest request, HttpServletResponse response)
        throws ServletException, IOException
 {
-   // Get session object
+   
+	
+	String action = request.getParameter("action");
+
+
+	if (action != null && action.equals("invalidate"))
+	   {  // Called from the invalidate button, kill the session.
+	      // Get session object
+	      HttpSession session = request.getSession();
+	      session.invalidate();
+
+	      response.setContentType("text/html");
+	      PrintWriter out = response.getWriter();
+
+	      out.println("<html>");
+	      out.println("<head>");
+	      out.println(" <title>Session lifecycle</title>");
+	      out.println("</head>");
+	      out.println("");
+	      out.println("<body>");
+
+	      out.println("<p>Your session has been invalidated.</P>");
+
+	      // Create a link so the user can create a new session.
+	      // The link will have a parameter builtin
+	      String lifeCycleURL = "/offutt/servlet/sessionLifeCycle";
+	      out.println("<a href=\"" + lifeCycleURL + "?action=newSession\">");
+	      out.println("Create new session</A>");
+
+	      out.println("</body>");
+	      out.println("</html>");
+	      out.close();
+	   }
+	   
+	   
+	   
+	else {
+	
+	 
+	
+	// Get session object
    HttpSession session = request.getSession();
 
    String name   = request.getParameter("attrib_name");
    String value  = request.getParameter("attrib_value");
+   String name2   = request.getParameter("attrib_name2");
+   String value2  = request.getParameter("attrib_value2");
    String remove = request.getParameter("attrib_remove");
 
    if (remove != null && remove.equals("on"))
@@ -60,10 +102,19 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.println(" Value: ");
    out.println(" <input type=\"text\" size=\"10\" name=\"attrib_value\">");
 
+   out.println("<br> Name: ");
+   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_name\">");
+
+   out.println(" Value: ");
+   out.println(" <input type=\"text\" size=\"10\" name=\"attrib_value\">");
+   
    out.println(" <br><input type=\"checkbox\" name=\"attrib_remove\">Remove");
    out.println(" <input type=\"submit\" name=\"update\" value=\"Update\">");
    out.println("</form>");
    out.println("<hr>");
+   
+   out.print  ("<br><br><a href=\"/attributeServlet?action=invalidate\">");
+   out.println("Invalidate the session</a>");
 
    out.println("Attributes in this session:");
    Enumeration e = session.getAttributeNames();
@@ -81,5 +132,6 @@ public void doGet (HttpServletRequest request, HttpServletResponse response)
    out.println("</body>");
    out.println("</html>");
    out.close();
+	}
 } // End doGet
 } //End  SessionLifeCycle
